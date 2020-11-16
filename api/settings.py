4f309c9ac66999f3574cf98e3a79bec0f3c80149
Ali Ibrahim <ai9sk@virginia.edu>
2020-11-16 18:44:48 -0500

    added requirements and procfile for heroku deployment

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MODELS = os.path.join(BASE_DIR, 'predictor/models')
+django_heroku.settings(locals())
